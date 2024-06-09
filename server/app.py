62df7e12334b8716a14df8672a39ce56f897c754
Monica Garcia <monicairenegarcia@gmail.com>
2024-06-08 18:09:18 -0700

    complete chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,60 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST', 'PATCH'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.all()
+        message_list = []
+        for message in messages:
+            message_list.append(message.to_dict())
+        
+        response = make_response(message_list, 200)
+        return response
+    elif request.method == 'POST':
+        print(request)
+        request_message = request.get_json()
+        print(request_message)
+        new_message = Message(request_message['body'], request_message['username'])
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        new_message_dict = new_message.to_dict()
+        print(new_message_dict)
+        response = make_response(new_message_dict, 201)
+        return response
+    elif request.method == 'PATCH':
+        response = make_response("Hello", 200)
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    
+    if request.method == 'GET':
+        response = make_response(message.to_dict(), 200)
+        return response
+    elif request.method == 'PATCH':
+        request_message = request.get_json()
+        if 'body' in request_message:
+            message.body = request_message['body']
+        if 'username' in request_message:
+            message.username = request_message['username']
+        db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+
+        response = make_response(message_dict, 200)
+        return response
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+        make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
